ce94703c922bce820dbc257994f001bf9b227cbf
a08037 <a08037@gmail.com>
2024-08-10 19:29:24 +0300

    add all cycle code

--- main.py
+++ main.py
@@ -1,26 +1,41 @@
 import pygame
 import random
 
-pygame.init
+pygame.init()
+
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.di
-splay.set_caption('Игра Тир')
+pygame.display.set_caption('Игра Тир')
 
 icon = pygame.image.load("img/1.jpg")
 pygame.display.set_icon(icon)
+
 target_image = pygame.image.load("img/target.png")
-target_width = 80
-target_height = 80
+target_width = target_image.get_width()
+target_height = target_image.get_height()
+
+# Изначальное расположение мишени
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
-color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
 
-
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 running = True
-while running
-    pass
-pygame.quit
+while running:
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x <= mouse_x <= target_x + target_width and target_y <= mouse_y <= target_y + target_height:
+                # Перемещение мишени на новое случайное место
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
+    # Отображение мишени на экране
+    screen.blit(target_image, (target_x, target_y))
+    pygame.display.update()
+
+pygame.quit()
